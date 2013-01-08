1a88df4b18e9c7e55ebb2e6c9cef5d3642b12325
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-01-08 12:01:44 +0100

    Adding custom value and type classes (experimental).
    Are not used currently.

--- src/main/scala/org/eclipse/imp/pdb/facts/impl/persistent/scala/StringValue.scala
+++ src/main/scala/org/eclipse/imp/pdb/facts/impl/persistent/scala/StringValue.scala
@@ -1,60 +1,86 @@
+/*******************************************************************************
+ * Copyright (c) 2012 CWI
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *
+ *   * Michael Steindorfer - Michael.Steindorfer@cwi.nl - CWI  
+ *******************************************************************************/
 package org.eclipse.imp.pdb.facts.impl.persistent.scala
 
 import org.eclipse.imp.pdb.facts.IString
+import org.eclipse.imp.pdb.facts.`type`.TypeFactory
+import org.eclipse.imp.pdb.facts.visitors.IValueVisitor
+import scala.collection.mutable.StringBuilder
+import org.eclipse.imp.pdb.facts.impl.persistent.scala.`type`.StringType
 
 // TODO: make value class (i.e. extend from AnyVal)
-abstract class StringValue extends IString {
+case class StringValue(val content: String) extends Value with IString {
 
-	/**
-	 * @return the Java string that this string represents
-	 */
-    def getValue: String;
+  override def t = StringType
 
-    /**
-     * Concatenates two strings
-     * @param other
-     * @return
-     */
-    def concat(other: IString): IString;
-    
-    /**
-     * Reverses a string
-     */
-    def reverse: IString;
+  def accept[T](v: IValueVisitor[T]): T = v visitString this
 
-    /**
-     * Computes the length of the string 
-     * @return amount of Unicode characters 
-     */
-    def length: Int;
-    
-    /**
-     * Computes a substring
-     *  
-     * @param start the inclusive start index
-     * @param end   the exclusive end index
-     */
-    def substring(start: Int, end: Int): IString;
-    
-    /**
-     * Computes a substring
-     *  
-     * @param start the inclusive start index
-     */
-    def substring(start: Int): IString ;
-    
-    /**
-     * Compares two strings lexicographically
-     * @param other
-     * @return -1 if receiver is less than other, 0 is receiver is equal, 1 if receiver is larger
-     */
-    def compare(other: IString): Int;
-    
-    /**
-     * Returns the Unicode character at the given index.
-     * @param index
-     * @return
-     */
-    def charAt(index: Int): Int;  
-  
+  /**
+   * @return the Java string that this string represents
+   */
+  def getValue: String = content
+
+  /**
+   * Concatenates two strings
+   * @param other
+   * @return
+   */
+  def concat(other: IString): IString = other match {
+    case that: StringValue => new StringValue(this.content ++ that.content)
+  }
+
+  /**
+   * Reverses a string
+   */
+  def reverse: IString = new StringValue(new StringBuilder(content).reverse toString)
+
+  /**
+   * Computes the length of the string
+   * @return amount of Unicode characters
+   */
+  def length: Int = content.codePointCount(0, content.length)
+
+  /**
+   * Computes a substring
+   *
+   * @param start the inclusive start index
+   * @param end   the exclusive end index
+   */
+  def substring(start: Int, end: Int): IString = new StringValue(
+      content.substring(
+          content.offsetByCodePoints(0, start), 
+          content.offsetByCodePoints(0, end)))
+
+  /**
+   * Computes a substring
+   *
+   * @param start the inclusive start index
+   */
+  def substring(start: Int): IString = new StringValue(content.substring(start))
+
+  /**
+   * Compares two strings lexicographically
+   * @param other
+   * @return -1 if receiver is less than other, 0 is receiver is equal, 1 if receiver is larger
+   */
+  def compare(other: IString): Int = other match {
+    case that: StringValue => this.content compare that.content
+  }
+
+  /**
+   * Returns the Unicode character at the given index.
+   * @param index
+   * @return
+   */
+  def charAt(index: Int): Int = content.codePointAt(content.offsetByCodePoints(0, index))
+
 }
