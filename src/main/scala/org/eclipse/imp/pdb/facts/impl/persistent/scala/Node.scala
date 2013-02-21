604a57fd238a3c423b55038ce0ff697a82d4e1e9
Michael Steindorfer <Michael.Steindorfer@cwi.nl>
2013-02-21 14:55:34 +0100

    Relying on case class generated hashCode and equals. Removing type from node signature.

--- src/main/scala/org/eclipse/imp/pdb/facts/impl/persistent/scala/Node.scala
+++ src/main/scala/org/eclipse/imp/pdb/facts/impl/persistent/scala/Node.scala
@@ -22,11 +22,11 @@ import collection.JavaConversions.mapAsJavaMap
 import collection.JavaConversions.mapAsScalaMap
 import org.eclipse.imp.pdb.facts.IList
 
-sealed trait Node extends Value with INode {
+sealed trait Node extends INode {
   
   def name: String
   
-  def children: collection.immutable.List[IValue]
+  def children: Node.ChildrenColl
   
   def get(i: Int) = children(i)
 
@@ -43,23 +43,6 @@ sealed trait Node extends Value with INode {
   
   
   def replace(first: Int, second: Int, end: Int, repl: IList) = ???
-
-  
-  
-  override def hashCode = {
-    val hashFormula = (h: Int, x: IValue) => (h << 1) ^ (h >> 1) ^ x.hashCode
-    children.foldLeft(name.hashCode)(hashFormula)
-  }
-
-  override def equals(that: Any): Boolean = that match {
-    case other: Node => {
-      (this.t == other.t) &&
-        (this.children.length == other.children.length) &&
-        (this.name == other.name) &&
-        (0 until children.length).forall(i => this.children(i) equals other.children(i))
-    }
-    case _ => false
-  }
   
 }
 
@@ -71,24 +54,26 @@ object Node {
   type AnnotationsColl = collection.immutable.Map[String, IValue]
   val emptyAnnotations = collection.immutable.Map.empty[String, IValue]  
   
-  def apply(name: String) = SimpleNode(TypeFactory.getInstance.nodeType, name, emptyChildren)
+  def apply(name: String) = SimpleNode(name, emptyChildren)
 
-  def apply(name: String, children: ChildrenColl) = SimpleNode(TypeFactory.getInstance.nodeType, name, children)
+  def apply(name: String, children: ChildrenColl) = SimpleNode(name, children)
    
-  def apply(name: String, annotations: AnnotationsColl, children: ChildrenColl) = AnnotatedNode(TypeFactory.getInstance.nodeType, name, children, annotations)
+  def apply(name: String, children: ChildrenColl, annotations: AnnotationsColl) = AnnotatedNode(name, children, annotations)
   
 }
 
-case class SimpleNode(override val t: Type, val name: String, val children: Node.ChildrenColl)
+case class SimpleNode(val name: String, val children: Node.ChildrenColl)
   extends Value with Node {
   
-  def set(i: Int, x: IValue) = SimpleNode(t, name, children updated (i, x))
+  override val t = TypeFactory.getInstance.nodeType
+    
+  def set(i: Int, x: IValue) = SimpleNode(name, children updated (i, x))
   
-  def setAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(t, name, children, Node.emptyAnnotations ++ newAnnotations)
+  def setAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(name, children, Node.emptyAnnotations ++ newAnnotations)
 
-  def joinAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(t, name, children, Node.emptyAnnotations ++ newAnnotations)
+  def joinAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(name, children, Node.emptyAnnotations ++ newAnnotations)
 
-  def setAnnotation(label: String, newValue: IValue) = AnnotatedNode(t, name, children, Node.emptyAnnotations + (label -> newValue))
+  def setAnnotation(label: String, newValue: IValue) = AnnotatedNode(name, children, Node.emptyAnnotations + (label -> newValue))
 
   def hasAnnotations = false
 
@@ -104,18 +89,20 @@ case class SimpleNode(override val t: Type, val name: String, val children: Node
     
 }
 
-case class AnnotatedNode(override val t: Type, val name: String, val children: Node.ChildrenColl, val annotations: Node.AnnotationsColl)
-  extends Node {
+case class AnnotatedNode(val name: String, val children: Node.ChildrenColl, val annotations: Node.AnnotationsColl)
+  extends Value with Node {
   
   require (annotations.isEmpty == false)  
   
-  def set(i: Int, x: IValue) = AnnotatedNode(t, name, children updated (i, x), annotations)
+  override val t = TypeFactory.getInstance.nodeType  
+  
+  def set(i: Int, x: IValue) = AnnotatedNode(name, children updated (i, x), annotations)
     
-  override def setAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(t, name, children, Node.emptyAnnotations ++ newAnnotations)
+  override def setAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(name, children, Node.emptyAnnotations ++ newAnnotations)
 
-  override def joinAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(t, name, children, Node.emptyAnnotations ++ newAnnotations)
+  override def joinAnnotations(newAnnotations: java.util.Map[String, IValue]) = AnnotatedNode(name, children, Node.emptyAnnotations ++ newAnnotations)
 
-  override def setAnnotation(label: String, newValue: IValue) = AnnotatedNode(t, name, children, Node.emptyAnnotations + (label -> newValue))
+  override def setAnnotation(label: String, newValue: IValue) = AnnotatedNode(name, children, Node.emptyAnnotations + (label -> newValue))
 
   def hasAnnotations = true
 
@@ -125,10 +112,10 @@ case class AnnotatedNode(override val t: Type, val name: String, val children: N
   
   def getAnnotation(label: String) = annotations.getOrElse(label, null)
 
-  def removeAnnotations = SimpleNode(t, name, children)  
+  def removeAnnotations = SimpleNode(name, children)  
 
   def removeAnnotation(key: String) = (annotations - key) match {
-    case newAnnotations => if(newAnnotations.isEmpty) SimpleNode(t, name, children) else AnnotatedNode(t, name, children, newAnnotations)
+    case newAnnotations => if(newAnnotations.isEmpty) SimpleNode(name, children) else AnnotatedNode(name, children, newAnnotations)
   }
     
 }
